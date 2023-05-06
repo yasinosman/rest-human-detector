a5d9792d0e069452b98f3575d8dae6623899394c
Yasin Osman <yasinosman10@gmail.com>
2023-05-07 01:44:25 +0300

    feat: implement web server

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import os
-from detect_human import detect_human
-
-file_name = os.path.join(os.path.dirname(__file__), 'assets/gym3.jpg')
-assert os.path.exists(file_name)
-
-should_display = True
-
-detect_human(file_name, should_display)
